12281f371b62db2acae556c1c92c699ecebf5f79
ClydeLartey97 <lartey.d@etoncollege.org.uk>
2022-02-07 15:25:31 +0000

    Initial Commit

---
+++ AbsenceRecorder2/AbsenceRecorder2App.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorder2App.swift
+//  AbsenceRecorder2
+//
+//  Created by Clyde Lartey on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorder2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AbsenceRecorder2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AbsenceRecorder2
+//
+//  Created by Clyde Lartey on 07/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
